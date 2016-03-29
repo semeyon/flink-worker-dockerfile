97d9a46e514ad370503dd66d33ee362a5e65c27d
smartpeople <semeyon@speople.pro>
2016-03-29 14:49:52 +0600

    Fixies for auto connect and AKKA_FRAMESIZE

--- 1.0.0/start-flink-taskmanager.sh
+++ 1.0.0/start-flink-taskmanager.sh
@@ -2,9 +2,16 @@
 set -x
 #Replace the FLINK_MASTER_PROPERTY=jobmanager.rpc.address with FLINK_MASTER_IP(default=127.0.0.1)
 cat $FLINK_DIRECTORY/conf/flink-conf.yaml \
-| yaml-change $FLINK_MASTER_PROPERTY $FLINK_MASTER_IP > $FLINK_DIRECTORY/conf/flink-conf-new.yaml && \
+| yaml-change $FLINK_MASTER_PROPERTY $FLINK_MASTER_HOST > $FLINK_DIRECTORY/conf/flink-conf-new.yaml && \
 /bin/cp $FLINK_DIRECTORY/conf/flink-conf-new.yaml $FLINK_DIRECTORY/conf/flink-conf.yaml && \
 rm $FLINK_DIRECTORY/conf/flink-conf-new.yaml
+
+#Increase akka.framesize parameter to run jobs using flink cli
+echo $AKKA_FRAMESIZE: $AKKA_FRAMESIZE_VAl >> $FLINK_DIRECTORY/conf/flink-conf.yaml
+
+#Set flink master address
+truncate -s 0 $FLINK_DIRECTORY/conf/masters
+echo $FLINK_MASTER_HOST:8081 >> $FLINK_DIRECTORY/conf/masters
 
 # Start the flink task manager (slave)
 $FLINK_DIRECTORY/bin/taskmanager.sh start
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Flink worker (taskmanager)
+# Do not forget to setup the ENV FLINK_MASTER_HOST to automatically connect to master node
